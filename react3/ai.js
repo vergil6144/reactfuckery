1e13388110621ebd4e82dbb52e6c918186fe51eb
vergil6144 <adityadas0807@gmail.com>
2025-06-26 02:02:10 +0530

    keyfix

--- react3/ai.js
+++ react3/ai.js
@@ -1,5 +1,6 @@
 import { HfInference, InferenceClient } from '@huggingface/inference'
 import dotenv from 'dotenv';
+import apikey from './key';
 
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
@@ -20,7 +21,7 @@ You are an assistant that receives a list of ingredients that a user has and sug
 
 // Make sure you set an environment variable in Scrimba 
 // for HF_ACCESS_TOKEN
-const hf = new InferenceClient("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")
+const hf = new InferenceClient(apikey)
 export async function getRecipeFromMistral(ingredientsArr) {
     console.log(import.meta.env.HF)
     const ingredientsString = ingredientsArr.join(", ")
